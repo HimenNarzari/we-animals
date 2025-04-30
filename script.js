569b0c696f741a9b6c30e85bc0b60ef761c7e291
HimenNarzari <118021110+HimenNarzari@users.noreply.github.com>
2025-04-30 18:36:46 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+const counters = document.querySelectorAll('.count');
+
+const options = {
+  threshold: 0.5
+};
+
+const animateCount = (el) => {
+  const target = +el.getAttribute('data-target');
+  const speed = 50; // lower = faster
+
+  const update = () => {
+    const current = +el.innerText;
+    const increment = Math.ceil(target / speed);
+
+    if (current < target) {
+      el.innerText = current + increment;
+      setTimeout(update, 20);
+    } else {
+      el.innerText = target + "+";
+    }
+  };
+
+  update();
+};
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      animateCount(entry.target);
+      observer.unobserve(entry.target);
+    }
+  });
+}, options);
+
+counters.forEach(counter => {
+  observer.observe(counter);
+});
